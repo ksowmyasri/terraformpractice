4acc12e2cbec68cd89bbc453fe6ed0ee91f24a55
Ubuntu <ubuntu@ip-172-31-14-1.ap-south-1.compute.internal>
2021-02-22 10:35:47 +0000

    container resource added with internal and external port

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "docker_image" "nginx" {
 	name = "nginx:latest"
 }
 
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tdemo"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
